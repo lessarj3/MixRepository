d138e6e45d3daedd917b234f36709121b327eeca
lessaj3 <lessaj3@LESSAJ3-L1>
2017-10-17 10:54:25 -0500

    Cleaned up and removed extra files, changed home page, added ratio to detail section.

--- MixRepository/App_Start/RouteConfig.cs
+++ MixRepository/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MixRepository
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Mixes", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
